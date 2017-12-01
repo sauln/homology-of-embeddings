69b4910a2a470771e5f0c766b7dfa5ec86da9942
Nathaniel Saul <nat@saulgill.com>
2017-11-30 21:56:57 -0800

    organizing data and polishing pipeline

---
+++ generate_barcodes.sh
@@ -0,0 +1,34 @@
+NODE2VEC=~/Downloads/Snap-4.0/examples/node2vec
+RIPSER=~/research/libraries/ripser/ripser
+
+GRAPHROOT="data/graphs"
+BARCODEROOT="data/barcodes"
+
+TMPFILE="data/tmp"
+TMPFILE2="data/tmp2"
+
+
+#echo ""
+#echo "Generate graphs"
+#python generate_graphs.py
+
+for FILE in `ls data/graphs`
+do
+  echo ""
+  echo "Process $GRAPHROOT/$FILE"
+
+  echo "--- Run node2vec graph"
+  $NODE2VEC/node2vec -i:$GRAPHROOT/$FILE -o:$TMPFILE -l:20 -d:5 -p:1 -dr > /dev/null
+
+  python src/parse_ripser.py trim $TMPFILE $TMPFILE >/dev/null
+
+  echo "--- Compute barcode of embedding"
+  $RIPSER --format point-cloud --dim 1 $TMPFILE --output $TMPFILE2 >/dev/null
+
+  echo "--- Convert barcode to JSON"
+  python src/parse_ripser.py parse $TMPFILE2 "$BARCODEROOT/${FILE%.*}.json" >/dev/null
+
+  #python visualize.py $BARCODEFILE
+done
+
+rm $TMPFILE $TMPFILE2
--- src/parse_ripser.py
+++ src/parse_ripser.py
@@ -7,6 +7,7 @@ import numpy as np
 from more_itertools import flatten
 from utils import read_emb
 
+
 def ripser_to_dict(infile):
     print("Convert ripser output into json")
     with open(infile, "rb") as f:
@@ -19,11 +20,11 @@ def ripser_to_dict(infile):
         dim, start, end = line.split(" ")
         dim, start, end = float(dim), float(start), float(end)
 
-        barcode[dim].append( [start, end] )
+        barcode[dim].append([start, end])
 
     for dim, bars in barcode.items():
         largest = max(e for _, e in bars)
-        bars = [ [s,e] if e != -1 else [s, largest] for s,e in bars]
+        bars = [[s, e] if e != -1 else [s, largest] for s, e in bars]
         barcode[dim] = bars
 
     return barcode
@@ -33,44 +34,32 @@ def parse_barcode(infile, outfile):
     """ This converts the command line output from Ripser into JSON format.
     """
 
-
     print("Convert ripser output into json")
     with open(infile, "rb") as f:
         lines = f.readlines()
 
-    persistence_diagram = {}
-
-    max_range = float(lines[2].decode('utf-8').split(",")[1].replace("]", ""))
+    persistence_diagram = defaultdict(list)
 
     # find dim N start and finish
-    for line in lines[3:]:
+    for line in lines[:]:
         line = line.decode('utf-8')
-        if line.startswith("persistence"):
-            current_diagram = line.replace("persistence intervals in ", "").replace(":\n", "")
-            persistence_diagram[current_diagram] = []
-        else:
-            start, finish = line.split(",")
-            start = float(start.replace(" [", ""))
 
-            if finish[:2] == ' )':
-                finish = max_range
-            else:
-                finish = float(finish.replace(")\n", ""))
-
-            persistence_diagram[current_diagram].append([start, finish])
+        dim, start, finish = line.split(" ")
+        bar = [float(start), float(finish)]
+        persistence_diagram[int(dim)].append(bar)
 
     with open(outfile, 'w') as fp:
         json.dump(persistence_diagram, fp)
 
 
-def trim_ripser(infile, outfile):
+def trim_emb_for_ripser(infile, outfile):
     """ Convert the emb output from node2vec into a point-cloud
         so ripser can use it.
     """
 
-    print("Convert point-cloud to trimmed format for ripser")
+    print(f"Convert {infile} point-cloud to trimmed format for ripser")
     emb = read_emb(infile)
-    emb = emb[1:,]
+    emb = emb[1:, ]
 
     np.savetxt(outfile, emb, delimiter=" ")
 
@@ -80,12 +69,13 @@ def trim_ripser(infile, outfile):
 @click.argument('infile', default="barcodes/circle.bc")
 @click.argument('outfile', default="barcodes/circle_bc.json")
 def execute_formatting(action, infile, outfile):
+
     if action == "trim":
-        trim_ripser(infile, outfile)
+        trim_emb_for_ripser(infile, outfile)
     if action == "parse":
         parse_barcode(infile, outfile)
 
 
 if __name__ == "__main__":
-    #execute_formatting()
-    ripser_to_dict("barcodes/sphere.bc")
+    execute_formatting()
+    # ripser_to_dict("barcodes/sphere.bc")
